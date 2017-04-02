bb1b838d68ed6e49d5e4ec2261dc61a9e93dff0e
Sanjay Rai <rai7@live.co.uk>
2017-04-02 14:43:29 +0100

    Initial Commit

---
+++ PassingBetweenTableViewss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassingBetweenTableViewss
+//
+//  Created by Sanjay Rai on 02/04/2017.
+//  Copyright © 2017 Sanjay Rai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
